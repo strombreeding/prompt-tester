b294dda362b8caa0d14ae57ad4c194c3e032bb07
GigaJiny <jiny@imagineline.com>
2025-11-24 16:46:05 +0900

    delete 기능

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -87,6 +87,7 @@ export default function Main() {
                       },
                     }
                   );
+                  await reqFirst();
                 }}
               >
                 X
