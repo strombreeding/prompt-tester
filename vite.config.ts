d69ccd6b794b5e464f026237053051ea957e51c5
GigaJiny <jiny@imagineline.com>
2025-11-24 15:57:08 +0900

    config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ["giavanna-cherrylike-augustly.ngrok-free.dev"],
+  },
+  preview: {
+    allowedHosts: ["giavanna-cherrylike-augustly.ngrok-free.dev"],
+    host: true, // 또는 "0.0.0.0"
+  },
+  /* 귀찮으면 이렇게
+    preview: {
+    allowedHosts: true,
+    host: true,
+  },
+  */
   plugins: [react()],
-})
+});
